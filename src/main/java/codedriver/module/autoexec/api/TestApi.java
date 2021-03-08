30197f766eb00fde0b89223a87a85e834e4fd07e
Aienao <laiwt@techsure.com.cn>
2021-03-08 15:31:33 +0800

    first commit

---
+++ src/main/java/codedriver/module/autoexec/api/TestApi.java
@@ -0,0 +1,36 @@
+package codedriver.module.autoexec.api;
+
+import codedriver.framework.restful.annotation.Description;
+import codedriver.framework.restful.annotation.OperationType;
+import codedriver.framework.restful.constvalue.OperationTypeEnum;
+import codedriver.framework.restful.core.privateapi.PrivateApiComponentBase;
+import com.alibaba.fastjson.JSONObject;
+import org.springframework.stereotype.Service;
+
+@Service
+@OperationType(type = OperationTypeEnum.SEARCH)
+public class TestApi extends PrivateApiComponentBase{
+
+	@Override
+	public String getToken() {
+		return "autoexec/test";
+	}
+
+	@Override
+	public String getName() {
+		return "test";
+	}
+
+	@Override
+	public String getConfig() {
+		return null;
+	}
+
+	@Description(desc = "test")
+	@Override
+	public Object myDoService(JSONObject jsonObj) throws Exception {
+		JSONObject result = new JSONObject();
+		return "asdf";
+	}
+
+}
