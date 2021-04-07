99742c00a3b461949c7ea76067fa23d017f2fa1e
Aienao <laiwt@techsure.com.cn>
2021-04-07 12:02:36 +0800

    --story=1004298 --user=赖文韬 自动化-后端-脚本库-接... https://www.tapd.cn/54247054/s/1148518

---
+++ src/main/java/codedriver/module/autoexec/dto/AutoexecScriptValidateVo.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright(c) 2021. TechSure Co., Ltd. All Rights Reserved.
+ * 本内容仅限于深圳市赞悦科技有限公司内部传阅，禁止外泄以及用于其他的商业项目。
+ */
+
+package codedriver.module.autoexec.dto;
+
+import codedriver.framework.common.constvalue.ApiParamType;
+import codedriver.framework.common.dto.BaseEditorVo;
+import codedriver.framework.restful.annotation.EntityField;
+import codedriver.framework.util.SnowflakeUtil;
+
+public class AutoexecScriptValidateVo extends BaseEditorVo {
+
+    @EntityField(name = "id", type = ApiParamType.LONG)
+    private Long id;
+    @EntityField(name = "名称", type = ApiParamType.STRING)
+    private String name;
+    @EntityField(name = "危险代码", type = ApiParamType.STRING)
+    private String code;
+    @EntityField(name = "等级", type = ApiParamType.STRING)
+    private String level;
+
+    public AutoexecScriptValidateVo() {
+    }
+
+    public Long getId() {
+        if (id == null) {
+            id = SnowflakeUtil.uniqueLong();
+        }
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getLevel() {
+        return level;
+    }
+
+    public void setLevel(String level) {
+        this.level = level;
+    }
+}
---
+++ src/main/java/codedriver/module/autoexec/dto/AutoexecScriptVo.java
@@ -0,0 +1,82 @@
+/*
+ * Copyright(c) 2021. TechSure Co., Ltd. All Rights Reserved.
+ * 本内容仅限于深圳市赞悦科技有限公司内部传阅，禁止外泄以及用于其他的商业项目。
+ */
+
+package codedriver.module.autoexec.dto;
+
+import codedriver.framework.common.constvalue.ApiParamType;
+import codedriver.framework.common.dto.BaseEditorVo;
+import codedriver.framework.restful.annotation.EntityField;
+import codedriver.framework.util.SnowflakeUtil;
+
+public class AutoexecScriptVo extends BaseEditorVo {
+
+    @EntityField(name = "id", type = ApiParamType.LONG)
+    private Long id;
+    @EntityField(name = "唯一标识", type = ApiParamType.STRING)
+    private String name;
+    @EntityField(name = "名称", type = ApiParamType.STRING)
+    private String label;
+    @EntityField(name = "执行方式", type = ApiParamType.STRING)
+    private String execMode;
+    @EntityField(name = "分类ID", type = ApiParamType.LONG)
+    private Long typeId;
+    @EntityField(name = "操作级别ID", type = ApiParamType.LONG)
+    private Long riskId;
+
+    public AutoexecScriptVo() {
+    }
+
+    public Long getId() {
+        if (id == null) {
+            id = SnowflakeUtil.uniqueLong();
+        }
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+
+    public void setLabel(String label) {
+        this.label = label;
+    }
+
+    public String getExecMode() {
+        return execMode;
+    }
+
+    public void setExecMode(String execMode) {
+        this.execMode = execMode;
+    }
+
+    public Long getTypeId() {
+        return typeId;
+    }
+
+    public void setTypeId(Long typeId) {
+        this.typeId = typeId;
+    }
+
+    public Long getRiskId() {
+        return riskId;
+    }
+
+    public void setRiskId(Long riskId) {
+        this.riskId = riskId;
+    }
+
+}
