981dbbe59b1a5a731d4e1929ef0d8d9128888b5d
赖文韬 <laiwt@techsure.com.cn>
2021-04-13 10:48:05 +0800

    --story=1004254 --user=赖文韬 自动化-后端-脚本库 https://www.tapd.cn/54247054/s/1149299

--- src/main/java/codedriver/module/autoexec/api/script/AutoexecScriptBaseInfoSaveApi.java
+++ src/main/java/codedriver/module/autoexec/api/script/AutoexecScriptBaseInfoSaveApi.java
@@ -15,7 +15,7 @@ import codedriver.framework.restful.annotation.*;
 import codedriver.framework.restful.constvalue.OperationTypeEnum;
 import codedriver.framework.restful.core.privateapi.PrivateApiComponentBase;
 import codedriver.module.autoexec.dao.mapper.AutoexecScriptMapper;
-import codedriver.module.autoexec.servcie.AutoexecScriptService;
+import codedriver.module.autoexec.service.AutoexecScriptService;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import org.springframework.stereotype.Service;
--- src/main/java/codedriver/module/autoexec/service/AutoexecScriptServiceImpl.java
+++ src/main/java/codedriver/module/autoexec/service/AutoexecScriptServiceImpl.java
@@ -3,7 +3,7 @@
  * 本内容仅限于深圳市赞悦科技有限公司内部传阅，禁止外泄以及用于其他的商业项目。
  */
 
-package codedriver.module.autoexec.servcie;
+package codedriver.module.autoexec.service;
 
 import codedriver.framework.autoexec.dto.AutoexecScriptVersionVo;
 import codedriver.framework.autoexec.dto.AutoexecScriptVo;
