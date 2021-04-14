612fa9cb382cbbe1a41c5fb13c4f0c27ab943175
赖文韬 <laiwt@techsure.com.cn>
2021-04-14 16:13:36 +0800

    --story=1004254 --user=赖文韬 自动化-后端-脚本库 https://www.tapd.cn/54247054/s/1149299

--- src/main/java/codedriver/module/autoexec/api/script/AutoexecScriptReviewApi.java
+++ src/main/java/codedriver/module/autoexec/api/script/AutoexecScriptReviewApi.java
@@ -5,18 +5,28 @@
 
 package codedriver.module.autoexec.api.script;
 
+import codedriver.framework.asynchronization.threadlocal.UserContext;
 import codedriver.framework.auth.core.AuthAction;
+import codedriver.framework.autoexec.constvalue.ScriptOperate;
+import codedriver.framework.autoexec.constvalue.ScriptVersionStatus;
+import codedriver.framework.autoexec.dto.script.AutoexecScriptAuditVo;
+import codedriver.framework.autoexec.dto.script.AutoexecScriptVersionVo;
+import codedriver.framework.autoexec.exception.AutoexecScriptVersionCannotReviewException;
+import codedriver.framework.autoexec.exception.AutoexecScriptVersionNotFoundException;
 import codedriver.framework.common.constvalue.ApiParamType;
 import codedriver.framework.restful.annotation.*;
 import codedriver.framework.restful.constvalue.OperationTypeEnum;
 import codedriver.framework.restful.core.privateapi.PrivateApiComponentBase;
 import codedriver.framework.autoexec.auth.AUTOEXEC_SCRIPT_REVIEW;
 import codedriver.module.autoexec.dao.mapper.AutoexecScriptMapper;
+import codedriver.module.autoexec.service.AutoexecScriptService;
 import com.alibaba.fastjson.JSONObject;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.Resource;
+import java.util.Objects;
 
 @Service
 @Transactional
@@ -26,6 +36,9 @@ public class AutoexecScriptReviewApi extends PrivateApiComponentBase {
 
     @Resource
     private AutoexecScriptMapper autoexecScriptMapper;
+
+    @Resource
+    private AutoexecScriptService autoexecScriptService;
 
     @Override
     public String getToken() {
@@ -45,13 +58,38 @@ public class AutoexecScriptReviewApi extends PrivateApiComponentBase {
     @Input({
             @Param(name = "versionId", type = ApiParamType.LONG, isRequired = true, desc = "脚本版本ID"),
             @Param(name = "action", type = ApiParamType.ENUM, rule = "pass,reject", isRequired = true, desc = "通过，驳回"),
-            @Param(name = "content", type = ApiParamType.STRING, desc = "描述")
+            @Param(name = "content", type = ApiParamType.STRING, desc = "驳回原因")
     })
     @Output({
     })
     @Description(desc = "审核脚本")
     @Override
     public Object myDoService(JSONObject jsonObj) throws Exception {
+        Long versionId = jsonObj.getLong("versionId");
+        String action = jsonObj.getString("action");
+        String content = jsonObj.getString("content");
+        AutoexecScriptVersionVo version = autoexecScriptMapper.getVersionByVersionId(versionId);
+        if (version == null) {
+            throw new AutoexecScriptVersionNotFoundException(versionId);
+        }
+        if (!Objects.equals(ScriptVersionStatus.SUBMITTED.getValue(), version.getStatus())) {
+            throw new AutoexecScriptVersionCannotReviewException();
+        }
+        AutoexecScriptVersionVo updateVo = new AutoexecScriptVersionVo();
+        updateVo.setId(versionId);
+        updateVo.setStatus(Objects.equals(ScriptOperate.PASS.getValue(), action)
+                ? ScriptVersionStatus.PASSED.getValue() : ScriptVersionStatus.REJECTED.getValue());
+        updateVo.setReviewer(UserContext.get().getUserUuid());
+        updateVo.setLcu(UserContext.get().getUserUuid());
+        autoexecScriptMapper.updateScriptVersion(updateVo);
+
+        JSONObject auditContent = new JSONObject();
+        auditContent.put("version", version.getVersion());
+        if (StringUtils.isNotBlank(content)) {
+            auditContent.put("content", content);
+        }
+        AutoexecScriptAuditVo auditVo = new AutoexecScriptAuditVo(version.getScriptId(), version.getId(), action, auditContent);
+        autoexecScriptService.audit(auditVo);
         return null;
     }
 
