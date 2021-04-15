b089ffcc139e366d5eb28c3fc3d7c22ec0fde222
林邦泉 <linbq@techsure.com.cn>
2021-04-15 11:36:30 +0800

    --story=1004261 --user=林邦泉 自动化-后端-组合工具 https://www.tapd.cn/54247054/s/1150303

---
+++ src/main/java/codedriver/module/autoexec/notify/handler/AutoexecCombopNotifyPolicyHandler.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright(c) 2021 TechSureCo.,Ltd.AllRightsReserved.
+ * 本内容仅限于深圳市赞悦科技有限公司内部传阅，禁止外泄以及用于其他的商业项目。
+ */
+
+package codedriver.module.autoexec.notify.handler;
+
+import codedriver.framework.autoexec.auth.AUTOEXEC_COMBOP_MODIFY;
+import codedriver.framework.dto.ConditionParamVo;
+import codedriver.framework.notify.core.INotifyPolicyHandlerGroup;
+import codedriver.framework.notify.core.NotifyHandlerType;
+import codedriver.framework.notify.core.NotifyPolicyHandlerBase;
+import codedriver.framework.notify.dto.NotifyTriggerTemplateVo;
+import codedriver.framework.notify.dto.NotifyTriggerVo;
+import com.alibaba.fastjson.JSONObject;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+/**
+ * @author: linbq
+ * @since: 2021/4/15 9:47
+ **/
+@Component
+public class AutoexecCombopNotifyPolicyHandler extends NotifyPolicyHandlerBase {
+    @Override
+    public String getName() {
+        return "组合工具";
+    }
+
+    /**
+     * 绑定权限，每种handler对应不同的权限
+     * @return
+     */
+    @Override
+    public String getAuthName() {
+        return AUTOEXEC_COMBOP_MODIFY.class.getSimpleName();
+    }
+
+    @Override
+    public INotifyPolicyHandlerGroup getGroup() {
+        return null;
+    }
+
+    @Override
+    protected List<NotifyTriggerVo> myNotifyTriggerList() {
+        return null;
+    }
+
+    @Override
+    protected List<NotifyTriggerTemplateVo> myNotifyTriggerTemplateList(NotifyHandlerType type) {
+        return null;
+    }
+
+    @Override
+    protected List<ConditionParamVo> mySystemParamList() {
+        return null;
+    }
+
+    @Override
+    protected List<ConditionParamVo> mySystemConditionOptionList() {
+        return null;
+    }
+
+    @Override
+    protected void myAuthorityConfig(JSONObject config) {
+
+    }
+}
