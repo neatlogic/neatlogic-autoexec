7e7a58f68ca5ab5cf9d04e4aca5c2da29b300dc2
赖文韬 <laiwt@techsure.com.cn>
2021-04-29 10:26:08 +0800

    --task=1004520 --user=赖文韬 后端-接口 https://www.tapd.cn/54247054/s/1152717

--- src/main/java/codedriver/module/autoexec/service/AutoexecServiceImpl.java
+++ src/main/java/codedriver/module/autoexec/service/AutoexecServiceImpl.java
@@ -8,12 +8,14 @@ package codedriver.module.autoexec.service;
 import codedriver.framework.autoexec.constvalue.ParamMode;
 import codedriver.framework.autoexec.constvalue.ParamType;
 import codedriver.framework.autoexec.dto.IParam;
+import codedriver.framework.autoexec.dto.script.AutoexecScriptVersionParamVo;
 import codedriver.framework.exception.type.ParamIrregularException;
 import codedriver.framework.exception.type.ParamNotExistsException;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.regex.Pattern;
 
 @Service
@@ -31,18 +33,27 @@ public class AutoexecServiceImpl implements AutoexecService {
         for (int i = 0; i < paramList.size(); i++) {
             IParam param = paramList.get(i);
             if (param != null) {
+                String mode = param.getMode();
                 String key = param.getKey();
+                String type = param.getType();
+                Integer isRequired = param.getIsRequired();
+                if (param instanceof AutoexecScriptVersionParamVo && StringUtils.isBlank(mode)) {
+                    throw new ParamNotExistsException("参数：“paramList.[" + i + "].mode”不能为空");
+                }
+                if (StringUtils.isNotBlank(mode)
+                        && !Objects.equals(ParamMode.INPUT.getValue(), mode)
+                        && !Objects.equals(ParamMode.OUTPUT.getValue(), mode)) {
+                    throw new ParamIrregularException("参数：“paramList.[" + i + "].mode”不符合格式要求");
+                }
                 if (StringUtils.isBlank(key)) {
                     throw new ParamNotExistsException("参数：“paramList.[" + i + "].key”不能为空");
                 }
                 if (!paramKeyPattern.matcher(key).matches()) {
                     throw new ParamIrregularException("参数：“paramList.[" + i + "].key”不符合格式要求");
                 }
-                Integer isRequired = param.getIsRequired();
-                if (isRequired == null && !ParamMode.OUTPUT.getValue().equals(param.getMode())) {
+                if (isRequired == null && !Objects.equals(ParamMode.OUTPUT.getValue(), mode)) {
                     throw new ParamNotExistsException("参数：“paramList.[" + i + "].isRequired”不能为空");
                 }
-                String type = param.getType();
                 if (StringUtils.isBlank(type)) {
                     throw new ParamNotExistsException("参数：“paramList.[" + i + "].type”不能为空");
                 }
