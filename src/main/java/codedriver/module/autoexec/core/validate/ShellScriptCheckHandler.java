88bdba70214de0fe33015253b7454004bffa82f4
赖文韬 <laiwt@techsure.com.cn>
2021-04-30 15:20:26 +0800

    --task=1004520 --user=赖文韬 后端-接口 https://www.tapd.cn/54247054/s/1153047

---
+++ src/main/java/codedriver/module/autoexec/core/validate/ShellScriptCheckHandler.java
@@ -0,0 +1,86 @@
+/*
+ * Copyright(c) 2021. TechSure Co., Ltd. All Rights Reserved.
+ * 本内容仅限于深圳市赞悦科技有限公司内部传阅，禁止外泄以及用于其他的商业项目。
+ */
+
+package codedriver.module.autoexec.core.validate;
+
+import codedriver.framework.autoexec.constvalue.ScriptParser;
+import codedriver.framework.autoexec.dto.script.AutoexecScriptLineVo;
+import codedriver.framework.autoexec.dto.script.AutoexecScriptValidateVo;
+import codedriver.framework.autoexec.scriptcheck.ScriptCheckHandlerBase;
+import codedriver.framework.autoexec.util.ArgumentTokenizer;
+import codedriver.module.autoexec.dao.mapper.AutoexecScriptValidateMapper;
+import org.apache.commons.collections4.CollectionUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+
+@Component
+public class ShellScriptCheckHandler extends ScriptCheckHandlerBase {
+
+    @Resource
+    private AutoexecScriptValidateMapper validateMapper;
+
+    @Override
+    protected void myCheck(List<AutoexecScriptLineVo> lineList) {
+        List<AutoexecScriptValidateVo> codeList = validateMapper.getCodeListByType(getType());
+        if (CollectionUtils.isNotEmpty(codeList)) {
+            for (AutoexecScriptLineVo lineVo : lineList) {
+                if (!Objects.equals(lineVo.getIsAnnotation(), 1)) {
+                    String content = lineVo.getContent();
+                    List<Map<String, String>> tokenize = ArgumentTokenizer.tokenize(content);
+                    if (CollectionUtils.isNotEmpty(tokenize)) {
+                        for (Map<String, String> map : tokenize) {
+                            if (Objects.equals(map.get("type"), "command")) {
+                                String value = map.get("value");
+                                if (StringUtils.isNotBlank(value)) {
+                                    // 可能存在多个等级的危险代码，取等级高的
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+    }
+
+    /**
+     * 标记注释行
+     *
+     * @param lineList
+     */
+    @Override
+    protected void markAnnotationLines(List<AutoexecScriptLineVo> lineList) {
+        lineList.stream().forEach(o -> {
+            if (o.getContent().startsWith("#")) {
+                o.setIsAnnotation(1);
+            }
+        });
+        // todo shell存在多种多行注释方式
+//        for (int i = 0; i < lineList.size(); i++) {
+//            if (lineList.get(i).getContent().startsWith("#")) {
+//                lineList.get(i).setIsAnnotation(1);
+//            } else if (lineList.get(i).getContent().startsWith(":<<!")) {
+//                while (lineList.get(i).getContent().startsWith(":<<!")
+//                        || !lineList.get(i).getContent().endsWith("!")) {
+//                    lineList.get(i).setIsAnnotation(1);
+//                    i++;
+//                    if (lineList.get(i).getContent().endsWith("!")) {
+//                        lineList.get(i).setIsAnnotation(1);
+//                    }
+//                }
+//            }
+//        }
+    }
+
+    @Override
+    public String getType() {
+        return ScriptParser.SHELL.getValue();
+    }
+}
