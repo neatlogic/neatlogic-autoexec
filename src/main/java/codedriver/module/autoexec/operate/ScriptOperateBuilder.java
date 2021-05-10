cfde8b470c0508830e6f88363d622de6d1a5c27f
赖文韬 <laiwt@techsure.com.cn>
2021-05-10 18:19:31 +0800

    --task=1004514 --user=赖文韬 后端 - 工具库search接... https://www.tapd.cn/54247054/s/1154049

--- src/main/java/codedriver/module/autoexec/operate/ScriptOperateBuilder.java
+++ src/main/java/codedriver/module/autoexec/operate/ScriptOperateBuilder.java
@@ -48,8 +48,7 @@ public class ScriptOperateBuilder {
     }
 
     public ScriptOperateBuilder setCopy() {
-        if (AuthActionChecker.checkByUserUuid(userUuid, AUTOEXEC_SCRIPT_REVIEW.class.getSimpleName())
-                || AuthActionChecker.checkByUserUuid(userUuid, AUTOEXEC_SCRIPT_MODIFY.class.getSimpleName())) {
+        if (AuthActionChecker.checkByUserUuid(userUuid, AUTOEXEC_SCRIPT_MODIFY.class.getSimpleName())) {
             operateList.add(new ValueTextVo("copy", "复制"));
         }
         return this;
@@ -63,9 +62,7 @@ public class ScriptOperateBuilder {
     }
 
     public ScriptOperateBuilder setCompare() {
-        if (AuthActionChecker.checkByUserUuid(userUuid, AUTOEXEC_SCRIPT_USE.class.getSimpleName())
-                || AuthActionChecker.checkByUserUuid(userUuid, AUTOEXEC_SCRIPT_REVIEW.class.getSimpleName())
-                || AuthActionChecker.checkByUserUuid(userUuid, AUTOEXEC_SCRIPT_MODIFY.class.getSimpleName())) {
+        if (AuthActionChecker.checkByUserUuid(userUuid, AUTOEXEC_SCRIPT_USE.class.getSimpleName())) {
             operateList.add(new ValueTextVo("compare", "版本对比"));
         }
         return this;
@@ -73,11 +70,10 @@ public class ScriptOperateBuilder {
 
     public ScriptOperateBuilder setValidate() {
         // 拥有脚本审核或维护权限，且处于编辑中、已驳回、待审核状态才能校验
-        if ((AuthActionChecker.checkByUserUuid(userUuid, AUTOEXEC_SCRIPT_REVIEW.class.getSimpleName())
-                || AuthActionChecker.checkByUserUuid(userUuid, AUTOEXEC_SCRIPT_MODIFY.class.getSimpleName()))
+        if ((AuthActionChecker.checkByUserUuid(userUuid, AUTOEXEC_SCRIPT_MODIFY.class.getSimpleName()))
                 && (Objects.equals(ScriptVersionStatus.DRAFT.getValue(), status)
-                || Objects.equals(ScriptVersionStatus.REJECTED.getValue(), status))
-                || Objects.equals(ScriptVersionStatus.SUBMITTED.getValue(), status)) {
+                || Objects.equals(ScriptVersionStatus.REJECTED.getValue(), status)
+                || Objects.equals(ScriptVersionStatus.SUBMITTED.getValue(), status))) {
             operateList.add(new ValueTextVo("validate", "校验"));
         }
         return this;
@@ -85,8 +81,7 @@ public class ScriptOperateBuilder {
 
     public ScriptOperateBuilder setSave() {
         // 拥有脚本审核或维护权限，且处于编辑中、已驳回状态才能保存
-        if ((AuthActionChecker.checkByUserUuid(userUuid, AUTOEXEC_SCRIPT_REVIEW.class.getSimpleName())
-                || AuthActionChecker.checkByUserUuid(userUuid, AUTOEXEC_SCRIPT_MODIFY.class.getSimpleName()))
+        if ((AuthActionChecker.checkByUserUuid(userUuid, AUTOEXEC_SCRIPT_MODIFY.class.getSimpleName()))
                 && (Objects.equals(ScriptVersionStatus.DRAFT.getValue(), status)
                 || Objects.equals(ScriptVersionStatus.REJECTED.getValue(), status))) {
             operateList.add(new ValueTextVo("save", "保存"));
@@ -96,8 +91,7 @@ public class ScriptOperateBuilder {
 
     public ScriptOperateBuilder setSubmit() {
         // 拥有脚本审核或维护权限，且处于编辑中、已驳回状态才能提交审核
-        if ((AuthActionChecker.checkByUserUuid(userUuid, AUTOEXEC_SCRIPT_REVIEW.class.getSimpleName())
-                || AuthActionChecker.checkByUserUuid(userUuid, AUTOEXEC_SCRIPT_MODIFY.class.getSimpleName()))
+        if ((AuthActionChecker.checkByUserUuid(userUuid, AUTOEXEC_SCRIPT_MODIFY.class.getSimpleName()))
                 && (Objects.equals(ScriptVersionStatus.DRAFT.getValue(), status)
                 || Objects.equals(ScriptVersionStatus.REJECTED.getValue(), status))) {
             operateList.add(new ValueTextVo("submit", "提交审核"));
@@ -131,9 +125,7 @@ public class ScriptOperateBuilder {
     }
 
     public ScriptOperateBuilder setExport() {
-        if (AuthActionChecker.checkByUserUuid(userUuid, AUTOEXEC_SCRIPT_USE.class.getSimpleName())
-                || AuthActionChecker.checkByUserUuid(userUuid, AUTOEXEC_SCRIPT_REVIEW.class.getSimpleName())
-                || AuthActionChecker.checkByUserUuid(userUuid, AUTOEXEC_SCRIPT_MODIFY.class.getSimpleName())) {
+        if (AuthActionChecker.checkByUserUuid(userUuid, AUTOEXEC_SCRIPT_USE.class.getSimpleName())) {
             operateList.add(new ValueTextVo("export", "导出"));
         }
         return this;
