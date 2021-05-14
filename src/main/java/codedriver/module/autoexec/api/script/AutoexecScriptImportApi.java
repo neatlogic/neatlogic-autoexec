5dce844702768c298ca1bfbe4771baa49a65da9c
赖文韬 <laiwt@techsure.com.cn>
2021-05-14 10:18:46 +0800

    --task=1004635 --user=赖文韬 导出自定义工具-接口 https://www.tapd.cn/54247054/s/1153989

--- src/main/java/codedriver/module/autoexec/api/script/AutoexecScriptImportApi.java
+++ src/main/java/codedriver/module/autoexec/api/script/AutoexecScriptImportApi.java
@@ -6,29 +6,19 @@
 package codedriver.module.autoexec.api.script;
 
 import codedriver.framework.auth.core.AuthAction;
-import codedriver.framework.autoexec.auth.AUTOEXEC_SCRIPT_MODIFY;
-import codedriver.framework.autoexec.dto.script.AutoexecScriptVersionVo;
-import codedriver.framework.autoexec.dto.script.AutoexecScriptVo;
 import codedriver.framework.common.constvalue.ApiParamType;
-import codedriver.framework.exception.file.FileExtNotAllowedException;
-import codedriver.framework.exception.file.FileNotUploadException;
 import codedriver.framework.restful.annotation.*;
 import codedriver.framework.restful.constvalue.OperationTypeEnum;
 import codedriver.framework.restful.core.privateapi.PrivateBinaryStreamApiComponentBase;
+import codedriver.framework.autoexec.auth.AUTOEXEC_SCRIPT_MODIFY;
 import codedriver.module.autoexec.dao.mapper.AutoexecScriptMapper;
 import com.alibaba.fastjson.JSONObject;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.multipart.MultipartHttpServletRequest;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.util.List;
-import java.util.Map;
 
 @Service
 @Transactional
@@ -63,32 +53,6 @@ public class AutoexecScriptImportApi extends PrivateBinaryStreamApiComponentBase
     @Description(desc = "导入脚本")
     @Override
     public Object myDoService(JSONObject paramObj, HttpServletRequest request, HttpServletResponse response) throws Exception {
-        MultipartHttpServletRequest multipartRequest = (MultipartHttpServletRequest) request;
-        //获取所有导入文件
-        Map<String, MultipartFile> multipartFileMap = multipartRequest.getFileMap();
-        //如果没有导入文件, 抛异常
-        if (multipartFileMap == null || multipartFileMap.isEmpty()) {
-            throw new FileNotUploadException();
-        }
-        ObjectInputStream ois = null;
-        Object obj = null;
-        for (Map.Entry<String, MultipartFile> entry : multipartFileMap.entrySet()) {
-            MultipartFile multipartFile = entry.getValue();
-            try {
-                ois = new ObjectInputStream(multipartFile.getInputStream());
-                obj = ois.readObject();
-            } catch (IOException e) {
-                throw new FileExtNotAllowedException(multipartFile.getOriginalFilename());
-            } finally {
-                if (ois != null) {
-                    ois.close();
-                }
-            }
-            if(obj instanceof AutoexecScriptVo){
-                AutoexecScriptVo scriptVo = (AutoexecScriptVo) obj;
-                List<AutoexecScriptVersionVo> versionList = scriptVo.getVersionList();
-            }
-        }
         return null;
     }
 
