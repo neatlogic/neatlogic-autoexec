c6b7da7edf4debf4bd5e30feede3f3cffc88d609
吕佐康 <lvzk@techsure.com.cn>
2021-05-19 17:51:08 +0800

    --task=1004586 --user=lvzk 后端 - 执行作业 https://www.tapd.cn/54247054/s/1152545

--- src/main/java/codedriver/module/autoexec/api/job/AutoexecJobFromCombopCreateApi.java
+++ src/main/java/codedriver/module/autoexec/api/job/AutoexecJobFromCombopCreateApi.java
@@ -6,7 +6,7 @@
 package codedriver.module.autoexec.api.job;
 
 import codedriver.framework.auth.core.AuthAction;
-import codedriver.framework.autoexec.auth.AUTOEXEC_JOB_MODIFY;
+import codedriver.framework.autoexec.auth.AUTOEXEC_BASE;
 import codedriver.framework.autoexec.dto.combop.AutoexecCombopExecuteConfigVo;
 import codedriver.framework.autoexec.dto.combop.AutoexecCombopVo;
 import codedriver.framework.autoexec.dto.job.AutoexecJobVo;
@@ -36,7 +36,7 @@ import javax.annotation.Resource;
 
 @Transactional
 @Service
-@AuthAction(action = AUTOEXEC_JOB_MODIFY.class)
+@AuthAction(action = AUTOEXEC_BASE.class)
 @OperationType(type = OperationTypeEnum.CREATE)
 public class AutoexecJobFromCombopCreateApi extends PrivateApiComponentBase {
     @Resource
