4c035d0fff6122a1c8a0d4740cd363f03815e823
赖文韬 <laiwt@techsure.com.cn>
2021-05-25 10:34:43 +0800

    --story=1004354 --user=赖文韬 工具分类管理页 https://www.tapd.cn/54247054/s/1156303

---
+++ src/main/java/codedriver/module/autoexec/api/type/AutoexecTypeGetApi.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright(c) 2021. TechSure Co., Ltd. All Rights Reserved.
+ * 本内容仅限于深圳市赞悦科技有限公司内部传阅，禁止外泄以及用于其他的商业项目。
+ */
+
+package codedriver.module.autoexec.api.type;
+
+import codedriver.framework.auth.core.AuthAction;
+import codedriver.framework.autoexec.auth.AUTOEXEC_BASE;
+import codedriver.framework.autoexec.dto.AutoexecTypeVo;
+import codedriver.framework.autoexec.exception.AutoexecTypeNotFoundException;
+import codedriver.framework.common.constvalue.ApiParamType;
+import codedriver.framework.restful.annotation.*;
+import codedriver.framework.restful.constvalue.OperationTypeEnum;
+import codedriver.framework.restful.core.privateapi.PrivateApiComponentBase;
+import codedriver.module.autoexec.dao.mapper.AutoexecTypeMapper;
+import com.alibaba.fastjson.JSONObject;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+
+@Service
+@AuthAction(action = AUTOEXEC_BASE.class)
+@OperationType(type = OperationTypeEnum.SEARCH)
+public class AutoexecTypeGetApi extends PrivateApiComponentBase {
+
+    @Resource
+    private AutoexecTypeMapper autoexecTypeMapper;
+
+    @Override
+    public String getToken() {
+        return "autoexec/type/get";
+    }
+
+    @Override
+    public String getName() {
+        return "获取插件类型";
+    }
+
+    @Override
+    public String getConfig() {
+        return null;
+    }
+
+    @Input({
+            @Param(name = "id", type = ApiParamType.LONG, isRequired = true, desc = "类型ID"),
+    })
+    @Output({
+            @Param(explode = AutoexecTypeVo.class)
+    })
+    @Description(desc = "获取插件类型")
+    @Override
+    public Object myDoService(JSONObject jsonObj) throws Exception {
+        Long id = jsonObj.getLong("id");
+        AutoexecTypeVo type = autoexecTypeMapper.getTypeById(id);
+        if (type == null) {
+            throw new AutoexecTypeNotFoundException(id);
+        }
+        return type;
+    }
+
+
+}
