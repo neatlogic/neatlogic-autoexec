ca7b9a699b842b95fa1fab92aebef0dfb69c26ea
赖文韬 <laiwt@techsure.com.cn>
2021-07-08 15:25:25 +0800

    --task=1004983 --user=赖文韬 后端-增加描述字段 https://www.tapd.cn/54247054/s/1163128

---
+++ src/main/java/codedriver/module/autoexec/api/script/AutoexecScriptOrToolInputParamGetApi.java
@@ -0,0 +1,102 @@
+/*
+ * Copyright(c) 2021. TechSure Co., Ltd. All Rights Reserved.
+ * 本内容仅限于深圳市赞悦科技有限公司内部传阅，禁止外泄以及用于其他的商业项目。
+ */
+
+package codedriver.module.autoexec.api.script;
+
+import codedriver.framework.auth.core.AuthAction;
+import codedriver.framework.autoexec.auth.AUTOEXEC_BASE;
+import codedriver.framework.autoexec.constvalue.ParamMode;
+import codedriver.framework.autoexec.constvalue.ToolType;
+import codedriver.framework.autoexec.dto.AutoexecParamVo;
+import codedriver.framework.autoexec.dto.AutoexecToolVo;
+import codedriver.framework.autoexec.dto.script.AutoexecScriptVersionParamVo;
+import codedriver.framework.autoexec.exception.AutoexecScriptVersionNotFoundException;
+import codedriver.framework.autoexec.exception.AutoexecToolNotFoundException;
+import codedriver.framework.common.constvalue.ApiParamType;
+import codedriver.framework.restful.annotation.*;
+import codedriver.framework.restful.constvalue.OperationTypeEnum;
+import codedriver.framework.restful.core.privateapi.PrivateApiComponentBase;
+import codedriver.module.autoexec.dao.mapper.AutoexecScriptMapper;
+import codedriver.module.autoexec.dao.mapper.AutoexecToolMapper;
+import codedriver.module.autoexec.service.AutoexecService;
+import com.alibaba.fastjson.JSONObject;
+import org.apache.commons.collections4.CollectionUtils;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+@Service
+@AuthAction(action = AUTOEXEC_BASE.class)
+@OperationType(type = OperationTypeEnum.SEARCH)
+public class AutoexecScriptOrToolInputParamGetApi extends PrivateApiComponentBase {
+
+    @Resource
+    private AutoexecScriptMapper autoexecScriptMapper;
+
+    @Resource
+    private AutoexecToolMapper autoexecToolMapper;
+
+    @Resource
+    private AutoexecService autoexecService;
+
+    @Override
+    public String getToken() {
+        return "autoexec/scriptortool/inputparam/get";
+    }
+
+    @Override
+    public String getName() {
+        return "获取工具或自定义工具输入参数";
+    }
+
+    @Override
+    public String getConfig() {
+        return null;
+    }
+
+    @Input({
+            @Param(name = "id", type = ApiParamType.LONG, isRequired = true, desc = "工具ID或自定义工具版本ID"),
+            @Param(name = "type", type = ApiParamType.ENUM, rule = "script,tool", isRequired = true, desc = "工具或自定义工具"),
+    })
+    @Output({
+            @Param(name = "Return", explode = AutoexecParamVo[].class, desc = "输入参数"),
+    })
+    @Description(desc = "获取工具或自定义工具输入参数")
+    @Override
+    public Object myDoService(JSONObject jsonObj) throws Exception {
+        Long id = jsonObj.getLong("id");
+        String type = jsonObj.getString("type");
+        List<AutoexecParamVo> inputParamList = null;
+        if (ToolType.SCRIPT.getValue().equals(type)) {
+            if (autoexecScriptMapper.getVersionByVersionId(id) == null) {
+                throw new AutoexecScriptVersionNotFoundException(id);
+            }
+            List<AutoexecScriptVersionParamVo> paramList = autoexecScriptMapper.getParamListByVersionId(id);
+            if (CollectionUtils.isNotEmpty(paramList)) {
+                inputParamList = paramList.stream()
+                        .filter(o -> Objects.equals(o.getMode(), ParamMode.INPUT.getValue()))
+                        .sorted(Comparator.comparing(AutoexecScriptVersionParamVo::getSort))
+                        .collect(Collectors.toList());
+            }
+        } else {
+            AutoexecToolVo tool = autoexecToolMapper.getToolById(id);
+            if (tool == null) {
+                throw new AutoexecToolNotFoundException(id);
+            }
+            inputParamList = tool.getInputParamList();
+        }
+        if (CollectionUtils.isNotEmpty(inputParamList)) {
+            for (AutoexecParamVo autoexecParamVo : inputParamList) {
+                autoexecService.mergeConfig(autoexecParamVo);
+            }
+        }
+        return inputParamList;
+    }
+
+}
