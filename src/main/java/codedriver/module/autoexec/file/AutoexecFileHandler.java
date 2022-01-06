8807e9adda7307c360a3959f94a84a72e04c28d3
longrf <techsure901>
2022-01-06 16:46:08 +0800

    --story=1005769 --user=龙儒锋 附件下载权限认证-自动化 https://www.tapd.cn/54247054/s/1201252

--- src/main/java/codedriver/module/autoexec/file/AutoexecFileHandler.java
+++ src/main/java/codedriver/module/autoexec/file/AutoexecFileHandler.java
@@ -5,10 +5,21 @@
 
 package codedriver.module.autoexec.file;
 
+import codedriver.framework.auth.core.AuthActionChecker;
+import codedriver.framework.autoexec.auth.AUTOEXEC_BASE;
+import codedriver.framework.dao.mapper.UserMapper;
+import codedriver.framework.dto.UserAuthVo;
+import codedriver.framework.exception.type.PermissionDeniedException;
 import codedriver.framework.file.core.FileTypeHandlerBase;
 import codedriver.framework.file.dto.FileVo;
 import com.alibaba.fastjson.JSONObject;
+import com.alibaba.nacos.api.utils.StringUtils;
+import org.apache.commons.collections4.CollectionUtils;
 import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * 自动化模块附件处理器
@@ -18,9 +29,20 @@ import org.springframework.stereotype.Component;
  **/
 @Component
 public class AutoexecFileHandler extends FileTypeHandlerBase {
+
+    @Resource
+    private UserMapper userMapper;
+
     @Override
-    public boolean valid(String userUuid, FileVo fileVo, JSONObject jsonObj) {
-        return true;
+    public boolean valid(String userUuid, FileVo fileVo, JSONObject jsonObj) throws PermissionDeniedException {
+        List<UserAuthVo> userAuthVoList = userMapper.searchUserAllAuthByUserAuth(new UserAuthVo(userUuid));
+        if (CollectionUtils.isNotEmpty(userAuthVoList)) {
+            AuthActionChecker.getAuthList(userAuthVoList);
+            if (CollectionUtils.isNotEmpty(userAuthVoList.stream().filter(e -> StringUtils.equals(e.getAuth(), AUTOEXEC_BASE.class.getSimpleName())).collect(Collectors.toList()))) {
+                return true;
+            }
+        }
+        throw new PermissionDeniedException(AUTOEXEC_BASE.class);
     }
 
     @Override
