58f980ee3e20116f65138af88b86c10b07b1776a
lvzk <lvzk@techsure.com.cn>
2022-04-11 17:39:53 +0800

    update params.json output

--- src/main/java/codedriver/module/autoexec/service/AutoexecJobActionServiceImpl.java
+++ src/main/java/codedriver/module/autoexec/service/AutoexecJobActionServiceImpl.java
@@ -155,7 +155,7 @@ public class AutoexecJobActionServiceImpl implements AutoexecJobActionService, I
                                     if (CollectionUtils.isNotEmpty(param.getJSONArray("outputParamList"))) {
                                         for (Object arg : param.getJSONArray("outputParamList")) {
                                             JSONObject argJson = JSONObject.parseObject(arg.toString());
-                                            put(argJson.getString("key"), argJson.getString("value"));
+                                            put(argJson.getString("key"), argJson);
                                         }
                                     }
                                 }});
