43d84b39a1eef6854ffb559658aa58d30c545edd
lvzk <lvzk@techsure.com.cn>
2022-05-18 14:32:37 +0800

    update job refresh status

--- src/main/java/codedriver/module/autoexec/api/job/AutoexecJobDetailGetApi.java
+++ src/main/java/codedriver/module/autoexec/api/job/AutoexecJobDetailGetApi.java
@@ -109,11 +109,7 @@ public class AutoexecJobDetailGetApi extends PrivateApiComponentBase {
                 jobVo.setIsCanTakeOver(1);
             }
         }
-        JSONObject result = JSONObject.parseObject(JSONObject.toJSON(jobVo).toString());
-        //判断是否停止刷新作业详细
-        autoexecJobService.setIsRefresh(result, jobVo, jsonObj.getString("status"));
-        result.put("jobCurrentPhaseName", jobCurrentPhaseVo.getName());
-        return result;
+        return jobVo;
     }
 
     @Override
