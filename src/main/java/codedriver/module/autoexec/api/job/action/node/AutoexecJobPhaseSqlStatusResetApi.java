40d183f12efa5eb6d4fd0a46488c4aeac3dd515f
longrf <a5739119>
2022-05-18 16:23:44 +0800

    --task=1006294 --user=龙儒锋 后端-sql文件执行页面 reset重置状态接口添加重置sql文件状态的逻辑 https://www.tapd.cn/54247054/s/1228954

--- src/main/java/codedriver/module/autoexec/api/job/action/node/AutoexecJobPhaseSqlStatusResetApi.java
+++ src/main/java/codedriver/module/autoexec/api/job/action/node/AutoexecJobPhaseSqlStatusResetApi.java
@@ -1,16 +1,20 @@
 package codedriver.module.autoexec.api.job.action.node;
 
+import codedriver.framework.autoexec.dao.mapper.AutoexecJobMapper;
+import codedriver.framework.autoexec.dto.job.AutoexecJobVo;
+import codedriver.framework.autoexec.exception.AutoexecJobNotFoundException;
 import codedriver.framework.common.constvalue.ApiParamType;
-import codedriver.framework.exception.type.ParamNotExistsException;
+import codedriver.framework.crossover.CrossoverServiceFactory;
+import codedriver.framework.deploy.constvalue.JobSource;
+import codedriver.framework.deploy.crossover.IDeploySqlCrossoverMapper;
 import codedriver.framework.restful.annotation.Input;
 import codedriver.framework.restful.annotation.Output;
 import codedriver.framework.restful.annotation.Param;
 import codedriver.framework.restful.core.privateapi.PrivateApiComponentBase;
-import codedriver.module.autoexec.service.AutoexecService;
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
+import com.alibaba.nacos.api.utils.StringUtils;
 import org.apache.commons.collections4.CollectionUtils;
-import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
@@ -23,7 +27,7 @@ import javax.annotation.Resource;
 public class AutoexecJobPhaseSqlStatusResetApi extends PrivateApiComponentBase {
 
     @Resource
-    AutoexecService autoexecService;
+    AutoexecJobMapper autoexecJobMapper;
 
     @Override
     public String getName() {
@@ -42,33 +46,23 @@ public class AutoexecJobPhaseSqlStatusResetApi extends PrivateApiComponentBase {
 
     @Input({
             @Param(name = "jobId", type = ApiParamType.LONG, desc = "作业id", isRequired = true),
-            @Param(name = "jobPhaseName", type = ApiParamType.STRING, desc = "作业剧本名称"),
-            @Param(name = "sqlFileList", type = ApiParamType.JSONARRAY, desc = "sql文件列表"),
-            @Param(name = "jobPhaseNameList", type = ApiParamType.JSONARRAY, desc = "作业剧本名称列表")
+            @Param(name = "sqlIdList", type = ApiParamType.JSONARRAY, desc = "sql文件列表")
     })
     @Output({
     })
     @Override
     public Object myDoService(JSONObject paramObj) throws Exception {
-        //两种调用情景：
-        //1、当前作业下的某个阶段的sql文件批量重置  入参：jobId、jobPhaseName、sqlFileList
-        //2、当前作业的多个阶段重置               入参：jobId、jonPhaseNameList
-        Long jobId = paramObj.getLong("jobId");
-        JSONArray sqlFileArray = paramObj.getJSONArray("sqlFileList");
-        if (CollectionUtils.isNotEmpty(sqlFileArray)) {
-            //1、当前作业下的某个阶段的sql文件批量重置
-            String jobPhaseName = paramObj.getString("jobPhaseName");
-            if (StringUtils.isEmpty(jobPhaseName)) {
-                throw new ParamNotExistsException("jobPhaseName");
-            }
-            autoexecService.resetAutoexecJobSqlStatusByJobIdAndJobPhaseNameAndSqlFileList(jobId,jobPhaseName, sqlFileArray.toJavaList(String.class));
-        } else {
-            JSONArray jonPhaseNameArray = paramObj.getJSONArray("jobPhaseNameList");
-            if (CollectionUtils.isEmpty(jonPhaseNameArray)) {
-                throw new ParamNotExistsException("jobPhaseNameList");
-            }
-            //2、当前作业的多个阶段重置
-            autoexecService.resetAutoexecJobSqlStatusByJobIdAndJobPhaseNameList(paramObj.getLong("jobId"), jonPhaseNameArray.toJavaList(String.class));
+        AutoexecJobVo jobVo = autoexecJobMapper.getJobInfo(paramObj.getLong("jobId"));
+        if (jobVo == null) {
+            throw new AutoexecJobNotFoundException(paramObj.getLong("jobId"));
+        }
+        JSONArray sqlIdArray = paramObj.getJSONArray("sqlIdList");
+        if (CollectionUtils.isNotEmpty(sqlIdArray)) {
+            if (StringUtils.equals(JobSource.DEPLOY.getValue(), jobVo.getSource())) {
+                IDeploySqlCrossoverMapper iDeploySqlCrossoverMapper = CrossoverServiceFactory.getApi(IDeploySqlCrossoverMapper.class);
+                iDeploySqlCrossoverMapper.resetDeploySqlStatusBySqlIdList(sqlIdArray);
+            }else {
+                autoexecJobMapper.resetJobSqlStatusBySqlIdList(sqlIdArray);}
         }
         return null;
     }
