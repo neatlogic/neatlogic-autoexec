799a458fbc7cd2d39573e13219bf10282c18ab57
lvzk <lvzk@techsure.com.cn>
2022-05-19 19:40:53 +0800

    update autoexec

--- src/main/java/codedriver/module/autoexec/api/job/exec/AutoexecJobPhaseOperationScriptGetApi.java
+++ src/main/java/codedriver/module/autoexec/api/job/exec/AutoexecJobPhaseOperationScriptGetApi.java
@@ -71,35 +71,34 @@ public class AutoexecJobPhaseOperationScriptGetApi extends PublicApiComponentBas
         String operationId = jsonObj.getString("operationId");
         Long jobId = jsonObj.getLong("jobId");
         BigDecimal lastModified = null;
-        if(jsonObj.getDouble("lastModified") != null) {
+        if (jsonObj.getDouble("lastModified") != null) {
             lastModified = new BigDecimal(Double.toString(jsonObj.getDouble("lastModified")));
         }
-        Long opId = Long.valueOf(operationId.substring(operationId.lastIndexOf("_")+1));
+        Long opId = Long.valueOf(operationId.substring(operationId.lastIndexOf("_") + 1));
         AutoexecJobPhaseOperationVo jobPhaseOperationVo = autoexecJobMapper.getJobPhaseOperationByOperationId(opId);
-        if(jobPhaseOperationVo == null) {
+        if (jobPhaseOperationVo == null) {
             throw new AutoexecJobPhaseOperationNotFoundException(opId.toString());
         }
         AutoexecScriptVersionVo scriptVersionVoOld = autoexecScriptMapper.getVersionByVersionId(jobPhaseOperationVo.getVersionId());
         if (scriptVersionVoOld == null) {
-            throw new AutoexecScriptNotFoundException(jobPhaseOperationVo.getName()+":"+jobPhaseOperationVo.getVersionId());
+            throw new AutoexecScriptNotFoundException(jobPhaseOperationVo.getName() + ":" + jobPhaseOperationVo.getVersionId());
         }
         AutoexecScriptVersionVo scriptVersionVo = autoexecScriptMapper.getActiveVersionByScriptId(scriptVersionVoOld.getScriptId());
         if (scriptVersionVo == null) {
             throw new AutoexecScriptVersionHasNoActivedException(jobPhaseOperationVo.getName());
         }
-        if (lastModified != null) {
-            if (lastModified.multiply(new BigDecimal("1000")).longValue() >= scriptVersionVo.getLcd().getTime()) {
-                HttpServletResponse resp = ((ServletRequestAttributes) Objects.requireNonNull(RequestContextHolder.getRequestAttributes())).getResponse();
-                if (resp != null) {
-                    resp.setStatus(205);
-                    resp.getWriter().print(StringUtils.EMPTY);
-                }
+        if (lastModified != null && lastModified.multiply(new BigDecimal("1000")).longValue() >= scriptVersionVo.getLcd().getTime()) {
+            HttpServletResponse resp = ((ServletRequestAttributes) Objects.requireNonNull(RequestContextHolder.getRequestAttributes())).getResponse();
+            if (resp != null) {
+                resp.setStatus(205);
+                resp.getWriter().print(StringUtils.EMPTY);
             }
+        }else{
+            String script = autoexecCombopService.getOperationActiveVersionScriptByOperation(scriptVersionVo);
+            result.put("script", script);
+            //update job 对应operation version_id
+            autoexecJobMapper.updateJobPhaseOperationVersionIdByJobIdAndOperationId(scriptVersionVo.getId(), jobId, scriptVersionVo.getScriptId());
         }
-        String script = autoexecCombopService.getOperationActiveVersionScriptByOperation(scriptVersionVo);
-        result.put("script", script);
-        //update job 对应operation version_id
-        autoexecJobMapper.updateJobPhaseOperationVersionIdByJobIdAndOperationId(scriptVersionVo.getId(), jobId, scriptVersionVo.getScriptId());
         return result;
     }
 
