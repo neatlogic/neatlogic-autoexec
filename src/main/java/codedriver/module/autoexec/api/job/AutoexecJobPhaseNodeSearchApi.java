99832b395b596e4bf46bcfb28bc2b5d0f9e07a44
lvzk <lvzk@techsure.com.cn>
2022-06-01 11:58:00 +0800

    update node search api

--- src/main/java/codedriver/module/autoexec/api/job/AutoexecJobPhaseNodeSearchApi.java
+++ src/main/java/codedriver/module/autoexec/api/job/AutoexecJobPhaseNodeSearchApi.java
@@ -7,20 +7,19 @@ package codedriver.module.autoexec.api.job;
 
 import codedriver.framework.auth.core.AuthAction;
 import codedriver.framework.autoexec.auth.AUTOEXEC_BASE;
+import codedriver.framework.autoexec.constvalue.JobStatus;
 import codedriver.framework.autoexec.dao.mapper.AutoexecJobMapper;
 import codedriver.framework.autoexec.dto.job.AutoexecJobPhaseNodeVo;
 import codedriver.framework.autoexec.dto.job.AutoexecJobPhaseVo;
 import codedriver.framework.autoexec.dto.job.AutoexecJobVo;
 import codedriver.framework.autoexec.exception.AutoexecJobNotFoundException;
 import codedriver.framework.autoexec.exception.AutoexecJobPhaseNotFoundException;
-import codedriver.framework.cmdb.dao.mapper.resourcecenter.ResourceCenterMapper;
 import codedriver.framework.common.constvalue.ApiParamType;
 import codedriver.framework.common.dto.BasePageVo;
 import codedriver.framework.restful.annotation.*;
 import codedriver.framework.restful.constvalue.OperationTypeEnum;
 import codedriver.framework.restful.core.privateapi.PrivateApiComponentBase;
 import codedriver.framework.util.TableResultUtil;
-import codedriver.module.autoexec.service.AutoexecJobService;
 import com.alibaba.fastjson.JSONObject;
 import org.springframework.stereotype.Service;
 
@@ -40,15 +39,9 @@ public class AutoexecJobPhaseNodeSearchApi extends PrivateApiComponentBase {
     @Resource
     AutoexecJobMapper autoexecJobMapper;
 
-    @Resource
-    AutoexecJobService autoexecJobService;
-
-    @Resource
-    ResourceCenterMapper resourceCenterMapper;
-
     @Override
     public String getName() {
-        return "作业剧本节点搜索";
+        return "搜索作业剧本节点";
     }
 
     @Override
@@ -60,49 +53,38 @@ public class AutoexecJobPhaseNodeSearchApi extends PrivateApiComponentBase {
             @Param(name = "jobPhaseId", type = ApiParamType.LONG, desc = "作业剧本id", isRequired = true),
             @Param(name = "statusList", type = ApiParamType.JSONARRAY, desc = "作业状态"),
             @Param(name = "keyword", type = ApiParamType.STRING, desc = "关键词(节点名称或ip)", xss = true),
-            @Param(name = "status", type = ApiParamType.STRING, desc = "当前阶段状态"),
+            @Param(name = "isDelete", type = ApiParamType.STRING, desc = "是否删除"),
+            @Param(name = "nodeIdList", type = ApiParamType.JSONARRAY, desc = "作业阶段节点idList,用于刷新节点"),
             @Param(name = "currentPage", type = ApiParamType.INTEGER, desc = "当前页"),
             @Param(name = "pageSize", type = ApiParamType.INTEGER, desc = "每页数据条目"),
-            @Param(name = "needPage", type = ApiParamType.BOOLEAN, desc = "是否需要分页，默认true")
     })
     @Output({
             @Param(name = "tbodyList", type = ApiParamType.JSONARRAY, explode = AutoexecJobPhaseNodeVo[].class, desc = "列表"),
+            @Param(name = "status", type = ApiParamType.STRING, desc = "作业状态"),
+            @Param(name = "statusName", type = ApiParamType.STRING, desc = "作业状态名"),
             @Param(explode = BasePageVo.class),
-            @Param(name = "isRefresh", type = ApiParamType.INTEGER, isRequired = true, desc = "是否需要继续定时刷新，1:继续 0:停止")
     })
-    @Description(desc = "作业剧本节点搜索")
+    @Description(desc = "作业剧本节点搜索接口")
     @Override
     public Object myDoService(JSONObject jsonObj) throws Exception {
-        JSONObject result = new JSONObject();
         AutoexecJobPhaseNodeVo jobPhaseNodeVo = JSONObject.toJavaObject(jsonObj, AutoexecJobPhaseNodeVo.class);
         AutoexecJobPhaseVo phaseVo = autoexecJobMapper.getJobPhaseByPhaseId(jobPhaseNodeVo.getJobPhaseId());
-        if(phaseVo == null){
+        if (phaseVo == null) {
             throw new AutoexecJobPhaseNotFoundException(jobPhaseNodeVo.getJobPhaseId().toString());
         }
         AutoexecJobVo jobVo = autoexecJobMapper.getJobInfo(phaseVo.getJobId());
-        if(jobVo == null){
+        if (jobVo == null) {
             throw new AutoexecJobNotFoundException(phaseVo.getJobId().toString());
         }
         List<AutoexecJobPhaseNodeVo> jobPhaseNodeVoList = new ArrayList<>();
-        /*String protocol = jobPhaseNodeVoList.get(0).getProtocol();
-        String userName = jobPhaseNodeVoList.get(0).getUserName();
-        List<Long> resourceIdList = jobPhaseNodeVoList.stream().map(AutoexecJobPhaseNodeVo::getResourceId).collect(Collectors.toList());
-            List<AccountVo> accountVoList = resourceCenterMapper.getResourceAccountListByResourceIdAndProtocolAndAccount(resourceIdList, protocol,userName);
-            jobPhaseNodeVoList.forEach(o->{
-                List<AccountVo> accountVoTmpList = accountVoList.stream().filter(a-> Objects.equals(a.getResourceId(),o.getResourceId())).collect(Collectors.toList());
-                if(CollectionUtils.isNotEmpty(accountVoTmpList)) {
-                    o.setUserName(accountVoTmpList.get(0).getAccount());
-                    o.setPassword(accountVoTmpList.get(0).getPasswordPlain());
-                }
-            });*/
-        if (jobPhaseNodeVo.getNeedPage()) {
-            int rowNum = autoexecJobMapper.searchJobPhaseNodeCount(jobPhaseNodeVo);
-            jobPhaseNodeVo.setRowNum(rowNum);
-            if(rowNum >0){
-                jobPhaseNodeVoList =  autoexecJobMapper.searchJobPhaseNodeWithResource(jobPhaseNodeVo);
-            }
-            result = TableResultUtil.getResult(jobPhaseNodeVoList,jobPhaseNodeVo);
+        int rowNum = autoexecJobMapper.searchJobPhaseNodeCount(jobPhaseNodeVo);
+        jobPhaseNodeVo.setRowNum(rowNum);
+        if (rowNum > 0) {
+            jobPhaseNodeVoList = autoexecJobMapper.searchJobPhaseNodeWithResource(jobPhaseNodeVo);
         }
+        JSONObject result = TableResultUtil.getResult(jobPhaseNodeVoList, jobPhaseNodeVo);
+        result.put("status", jobVo.getStatus());
+        result.put("statusName", JobStatus.getText(jobVo.getStatus()));
         return result;
     }
 
