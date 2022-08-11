bc8388f8a9fc53a74cdf880e81821c3c23de37dd
lvzk <lvzk@techsure.com.cn>
2022-08-11 11:36:17 +0800

    add autoexec job check

--- src/main/java/codedriver/module/autoexec/api/job/action/CheckAutoexecJobApi.java
+++ src/main/java/codedriver/module/autoexec/api/job/action/CheckAutoexecJobApi.java
@@ -6,12 +6,18 @@
 package codedriver.module.autoexec.api.job.action;
 
 import codedriver.framework.auth.core.AuthAction;
-import codedriver.framework.autoexec.constvalue.JobStatus;
+import codedriver.framework.autoexec.constvalue.JobAction;
 import codedriver.framework.autoexec.dao.mapper.AutoexecJobMapper;
 import codedriver.framework.autoexec.dto.job.AutoexecJobVo;
+import codedriver.framework.autoexec.exception.AutoexecJobNotFoundException;
+import codedriver.framework.autoexec.job.action.core.AutoexecJobActionHandlerFactory;
+import codedriver.framework.autoexec.job.action.core.IAutoexecJobActionHandler;
 import codedriver.framework.common.constvalue.ApiParamType;
 import codedriver.framework.deploy.auth.DEPLOY_BASE;
-import codedriver.framework.restful.annotation.*;
+import codedriver.framework.restful.annotation.Description;
+import codedriver.framework.restful.annotation.Input;
+import codedriver.framework.restful.annotation.OperationType;
+import codedriver.framework.restful.annotation.Param;
 import codedriver.framework.restful.constvalue.OperationTypeEnum;
 import codedriver.framework.restful.core.privateapi.PrivateApiComponentBase;
 import com.alibaba.fastjson.JSONObject;
@@ -19,7 +25,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.Resource;
-import java.util.Objects;
 
 /**
  * @author lvzk
@@ -51,11 +56,12 @@ public class CheckAutoexecJobApi extends PrivateApiComponentBase {
     public Object myDoService(JSONObject jsonObj) throws Exception {
         Long jobId = jsonObj.getLong("jobId");
         AutoexecJobVo jobVo = autoexecJobMapper.getJobLockByJobId(jobId);
-        if (Objects.equals(jobVo.getStatus(), JobStatus.COMPLETED.getValue())) {
-            jobVo.setStatus(JobStatus.CHECKED.getValue());
-            autoexecJobMapper.updateJobStatus(jobVo);
+        if (jobVo == null) {
+            throw new AutoexecJobNotFoundException(jobId);
         }
-        return null;
+        jobVo.setAction(JobAction.CHECK.getValue());
+        IAutoexecJobActionHandler abortAction = AutoexecJobActionHandlerFactory.getAction(JobAction.CHECK.getValue());
+        return abortAction.doService(jobVo);
     }
 
     @Override
