757c5f4e95004fb07e2c5366f1850832004dda87
longrf <a5739119>
2022-10-08 11:17:38 +0800

    --story=1006918 --user=龙儒锋 自动化-工具库-增加参数说明导出功能 https://www.tapd.cn/54247054/s/1254506

--- src/main/java/codedriver/module/autoexec/api/job/test.java
+++ src/main/java/codedriver/module/autoexec/api/job/test.java
@@ -60,67 +60,20 @@ public class test extends PrivateBinaryStreamApiComponentBase {
             tableHeaderMap.put(3, "port2");
             tableHeaderMap.put(4, "port44");
             tableHeaderMap.put(5, "测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试");
-            //222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222
             Map<String, String> tableValueMap = new HashMap<>();
-//            tableValueMap.put("port", "22");
             tableValueMap.put("port", "23");
             tableValueMap.put("ip", "234.234.234.234");
-//            tableValueMap.put("ip", "192");
-
             wordBuilder.addTitle(TitleType.TILE, "测试");
             wordBuilder.addTitle(TitleType.H1, "测试").setFontSize(24).setAlignmentType(ParagraphAlignmentType.RIGHT);
             wordBuilder.addTitle(TitleType.H2, "测试").setFontFamily(FontFamily.FANG_SONG.getValue());
             wordBuilder.addTitle(TitleType.H3, "测试").setBold(false).setColor(FontColor.RED.getValue());
-
             wordBuilder.addBlankRow().addParagraph().setText("测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试")
                     .setCustom(false, FontColor.RED.getValue(), FontFamily.BLACK.getValue(), 12, 2, true, ParagraphAlignmentType.CENTER);
-
-
-            // 当前JVM占用的内存总数(M)
-            double total = (Runtime.getRuntime().totalMemory()) / (1024.0 * 1024);
-            System.out.println("当前JVM占用的内存总数(M)：" + total);
-            // JVM最大可用内存总数(M)
-            double max = (Runtime.getRuntime().maxMemory()) / (1024.0 * 1024);
-            System.out.println("JVM最大可用内存总数(M)：" + max);
-            // JVM空闲内存(M)
-            double free = (Runtime.getRuntime().freeMemory()) / (1024.0 * 1024);
-            System.out.println("JVM空闲内存(M)：" + free);
-            // 可用内存内存(M)
-            double mayuse = (max - total + free);
-            System.out.println("可用内存内存(M)：" + mayuse);
-            // 已经使用内存(M)
-            double used = (total - free);
-            System.out.println("已经使用内存(M)：" + used);
-
-//            wordBuilder.builder().write(os);
-//            wordBuilder.builder().close();
-//            wordBuilder.builder().getProperties().commit();
-            //            wordBuilder.addParagraph().setContext("测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试")
-//                    .setCustom(false, FontColor.BLUE.getValue(), FontFamily.SONG.getValue(), 12, 25, false, ParagraphAlignmentType.RIGHT);
-//            os.close();
-
-//            wordBuilder.addTable();
-
-//            wordBuilder.addParagraph("1213121312131213121312131213121312131213121312131213121312131213121312131213121312131213121312131213121312131213121312131213121312131213121312131213121312131213").setText("1213121312131213121312131213121312131213121312131213121312131213121312131213121312131213121312131213121312131213121312131213121312131213121312131213121312131213");
-//            wordBuilder.builder().write(os);
-
-
-//            OutputStream os1 = response.getOutputStream();
-//            WordBuilder wordBuilder1 = new WordBuilder();
-//            wordBuilder1.addBlankRow();
             wordBuilder.addTable().addTableHeaderMap(tableHeaderMap).setSpacingBetween(1.5);
-//                    .addRow(tableValueMap).addRow(tableValueMap);
-//
-//
-//            wordBuilder1.addParagraph("1213121312131213121312131213121312131213121312131213121312131213121312131213121312131213121312131213121312131213121312131213121312131213121312131213121312131213").setText("1213121312131213121312131213121312131213121312131213121312131213121312131213121312131213121312131213121312131213121312131213121312131213121312131213121312131213");
-//
             wordBuilder.builder().write(os);
             wordBuilder.builder().close();
-
-//            os1.close();
-//            stream.close();
-
-
+            os.flush();
+            os.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
