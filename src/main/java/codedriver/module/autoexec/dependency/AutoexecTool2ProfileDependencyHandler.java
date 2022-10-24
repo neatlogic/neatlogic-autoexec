5e5b251f504d5ca95cc8c9c4779c99765119ccbe
赖文韬 <laiwt@techsure.com.cn>
2022-10-24 15:19:05 +0800

    --story=1007048 --user=赖文韬 自动化-自定义工具-增加批量删除功能 https://www.tapd.cn/54247054/s/1258050

--- src/main/java/codedriver/module/autoexec/dependency/AutoexecTool2ProfileDependencyHandler.java
+++ src/main/java/codedriver/module/autoexec/dependency/AutoexecTool2ProfileDependencyHandler.java
@@ -1,12 +1,19 @@
 package codedriver.module.autoexec.dependency;
 
+import codedriver.framework.asynchronization.threadlocal.TenantContext;
 import codedriver.framework.autoexec.constvalue.AutoexecFromType;
+import codedriver.framework.autoexec.dao.mapper.AutoexecToolMapper;
+import codedriver.framework.autoexec.dto.AutoexecToolVo;
 import codedriver.framework.dependency.core.CustomTableDependencyHandlerBase;
 import codedriver.framework.dependency.core.IFromType;
 import codedriver.framework.dependency.dto.DependencyInfoVo;
+import com.alibaba.fastjson.JSONObject;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.Resource;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 /**
  * @author longrf
@@ -15,6 +22,10 @@ import java.util.List;
  */
 @Service
 public class AutoexecTool2ProfileDependencyHandler extends CustomTableDependencyHandlerBase {
+
+    @Resource
+    AutoexecToolMapper autoexecToolMapper;
+
     @Override
     protected String getTableName() {
         return "autoexec_profile_operation";
@@ -37,6 +48,24 @@ public class AutoexecTool2ProfileDependencyHandler extends CustomTableDependency
 
     @Override
     protected DependencyInfoVo parse(Object dependencyObj) {
+        if (dependencyObj == null) {
+            return null;
+        }
+        if (dependencyObj instanceof Map) {
+            Map<String, Object> map = (Map) dependencyObj;
+            Object operationId = map.get("operation_id");
+            if (operationId != null) {
+                AutoexecToolVo tool = autoexecToolMapper.getToolById(Long.valueOf(operationId.toString()));
+                if (tool != null) {
+                    JSONObject dependencyInfoConfig = new JSONObject();
+                    dependencyInfoConfig.put("toolId", tool.getId());
+                    List<String> pathList = new ArrayList<>();
+                    pathList.add("工具库");
+                    String urlFormat = "/" + TenantContext.get().getTenantUuid() + "/autoexec.html#/tool-detail?id=#{DATA.toolId}";
+                    return new DependencyInfoVo(tool.getId(), dependencyInfoConfig, tool.getName(), pathList, urlFormat, this.getGroupName());
+                }
+            }
+        }
         return null;
     }
 
