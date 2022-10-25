b39ac060e0a49c23a6c18980391bfa7ef4766574
赖文韬 <laiwt@techsure.com.cn>
2022-10-25 16:01:14 +0800

    --story=1007026 --user=赖文韬 自动化/自动发布-自定义工具/工具库-支持绑定【自定义模板】 https://www.tapd.cn/54247054/s/1258351

---
+++ src/main/java/codedriver/module/autoexec/api/job/GetAutoexecJobPhaseOperationLiveDataApi.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright (c)  2021 TechSure Co.,Ltd.  All Rights Reserved.
+ * 本内容仅限于深圳市赞悦科技有限公司内部传阅，禁止外泄以及用于其他的商业项目。
+ */
+
+package codedriver.module.autoexec.api.job;
+
+import codedriver.framework.auth.core.AuthAction;
+import codedriver.framework.autoexec.auth.AUTOEXEC_BASE;
+import codedriver.framework.autoexec.constvalue.JobAction;
+import codedriver.framework.autoexec.dto.job.AutoexecJobVo;
+import codedriver.framework.autoexec.job.action.core.AutoexecJobActionHandlerFactory;
+import codedriver.framework.autoexec.job.action.core.IAutoexecJobActionHandler;
+import codedriver.framework.common.constvalue.ApiParamType;
+import codedriver.framework.restful.annotation.Input;
+import codedriver.framework.restful.annotation.OperationType;
+import codedriver.framework.restful.annotation.Output;
+import codedriver.framework.restful.annotation.Param;
+import codedriver.framework.restful.constvalue.OperationTypeEnum;
+import codedriver.framework.restful.core.privateapi.PrivateApiComponentBase;
+import codedriver.module.autoexec.service.AutoexecJobService;
+import com.alibaba.fastjson.JSONObject;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+
+@Service
+@AuthAction(action = AUTOEXEC_BASE.class)
+@OperationType(type = OperationTypeEnum.SEARCH)
+public class GetAutoexecJobPhaseOperationLiveDataApi extends PrivateApiComponentBase {
+
+    @Resource
+    AutoexecJobService autoexecJobService;
+
+    @Override
+    public String getName() {
+        return "获取阶段工具个性化数据";
+    }
+
+    @Override
+    public String getConfig() {
+        return null;
+    }
+
+    @Input({
+            @Param(name = "jobPhaseId", type = ApiParamType.LONG, isRequired = true, desc = "作业剧本Id"),
+            @Param(name = "operationId", type = ApiParamType.LONG, isRequired = true, desc = "工具id"),
+            @Param(name = "resourceId", type = ApiParamType.LONG, desc = "资源Id"),
+            @Param(name = "sqlName", type = ApiParamType.STRING, desc = "sql名"),
+    })
+    @Output({
+    })
+    @Override
+    public Object myDoService(JSONObject paramObj) throws Exception {
+        AutoexecJobVo jobVo = new AutoexecJobVo();
+        jobVo.setActionParam(paramObj);
+        jobVo.setAction(JobAction.GET_OPERATION_LIVE_DATA.getValue());
+        IAutoexecJobActionHandler action = AutoexecJobActionHandlerFactory.getAction(JobAction.GET_OPERATION_LIVE_DATA.getValue());
+        return action.doService(jobVo);
+    }
+
+    @Override
+    public String getToken() {
+        return "/autoexec/job/phase/operation/livedata/get";
+    }
+}
