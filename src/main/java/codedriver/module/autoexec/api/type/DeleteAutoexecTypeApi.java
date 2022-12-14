a315ed4981f1b7bee5b1796cffd4780fa562e2dd
longrf <a5739119>
2022-12-14 10:45:09 +0800

    --story=1007195 --user=龙儒锋 自动发布 -- 流水线 -- 编辑 -- 添加工具时，工具列表的查询接口，增加工具分类的白名单过滤 https://www.tapd.cn/54247054/s/1264035

--- src/main/java/codedriver/module/autoexec/api/type/DeleteAutoexecTypeApi.java
+++ src/main/java/codedriver/module/autoexec/api/type/DeleteAutoexecTypeApi.java
@@ -7,9 +7,11 @@ package codedriver.module.autoexec.api.type;
 
 import codedriver.framework.auth.core.AuthAction;
 import codedriver.framework.autoexec.auth.AUTOEXEC_MODIFY;
+import codedriver.framework.autoexec.constvalue.AutoexecTypeType;
 import codedriver.framework.autoexec.dao.mapper.AutoexecTypeMapper;
 import codedriver.framework.autoexec.dto.AutoexecTypeVo;
 import codedriver.framework.autoexec.exception.AutoexecTypeHasBeenReferredException;
+import codedriver.framework.autoexec.exception.AutoexecTypeIsFactoryException;
 import codedriver.framework.autoexec.exception.AutoexecTypeNotFoundException;
 import codedriver.framework.common.constvalue.ApiParamType;
 import codedriver.framework.crossover.CrossoverServiceFactory;
@@ -18,6 +20,7 @@ import codedriver.framework.restful.annotation.*;
 import codedriver.framework.restful.constvalue.OperationTypeEnum;
 import codedriver.framework.restful.core.privateapi.PrivateApiComponentBase;
 import com.alibaba.fastjson.JSONObject;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -53,10 +56,13 @@ public class DeleteAutoexecTypeApi extends PrivateApiComponentBase {
     @Override
     public Object myDoService(JSONObject jsonObj) throws Exception {
         Long id = jsonObj.getLong("id");
-        if (autoexecTypeMapper.checkTypeIsExistsById(id) == 0) {
+        AutoexecTypeVo type = autoexecTypeMapper.getTypeById(id);
+        if (type == null) {
             throw new AutoexecTypeNotFoundException(id);
         }
-        AutoexecTypeVo type = autoexecTypeMapper.getTypeById(id);
+        if (StringUtils.equals(AutoexecTypeType.FACTORY.getValue(), type.getType())) {
+            throw new AutoexecTypeIsFactoryException(id, type.getName());
+        }
         // 已经被工具或脚本引用的分类不可删除
         if (autoexecTypeMapper.checkTypeHasBeenReferredById(id) > 0) {
             throw new AutoexecTypeHasBeenReferredException(type.getName());
