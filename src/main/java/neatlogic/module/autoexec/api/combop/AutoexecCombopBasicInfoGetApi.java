5d1c686709a31df0e0479aebdfcfcfa7d0445ee4
linbangquan <1437892690@qq.com>
2023-03-28 15:07:46 +0800

    --bug=1010816 组合工具草稿详情页一直loading https://www.tapd.cn/54247054/s/1269394

--- src/main/java/neatlogic/module/autoexec/api/combop/AutoexecCombopBasicInfoGetApi.java
+++ src/main/java/neatlogic/module/autoexec/api/combop/AutoexecCombopBasicInfoGetApi.java
@@ -24,10 +24,7 @@ import neatlogic.framework.autoexec.dao.mapper.AutoexecTypeMapper;
 import neatlogic.framework.autoexec.dto.AutoexecTypeVo;
 import neatlogic.framework.autoexec.dto.combop.AutoexecCombopAuthorityVo;
 import neatlogic.framework.autoexec.dto.combop.AutoexecCombopVo;
-import neatlogic.framework.autoexec.exception.AutoexecCombopDraftVersionNotFoundException;
-import neatlogic.framework.autoexec.exception.AutoexecCombopNotFoundException;
-import neatlogic.framework.autoexec.exception.AutoexecCombopRejectedVersionNotFoundException;
-import neatlogic.framework.autoexec.exception.AutoexecCombopSubmittedVersionNotFoundException;
+import neatlogic.framework.autoexec.exception.*;
 import neatlogic.framework.common.constvalue.ApiParamType;
 import neatlogic.framework.common.constvalue.GroupSearch;
 import neatlogic.framework.restful.annotation.*;
@@ -124,6 +121,9 @@ public class AutoexecCombopBasicInfoGetApi extends PrivateApiComponentBase {
         autoexecCombopVo.setActiveVersionId(activeVersionId);
         String versionStatus = jsonObj.getString("versionStatus");
         if (Objects.equals(versionStatus, ScriptVersionStatus.PASSED.getValue())) {
+            if (activeVersionId == null) {
+                throw new AutoexecCombopActiveVersionNotFoundException(autoexecCombopVo.getName());
+            }
             autoexecCombopVo.setSpecifyVersionId(activeVersionId);
         } else {
             Long maxVersionId = autoexecCombopVersionMapper.getAutoexecCombopMaxVersionIdByCombopIdAndStatus(id, versionStatus);
