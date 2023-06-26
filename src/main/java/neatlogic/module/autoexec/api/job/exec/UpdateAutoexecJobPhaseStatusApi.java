42dfcdb754efebfb7c82b875ec2de7f2e08b4981
lvzk <897706680@qq.com>
2023-06-26 10:37:19 +0800

    [修复]发布，sql失败重跑，commit成功后，作业状态还是失败

--- src/main/java/neatlogic/module/autoexec/api/job/exec/UpdateAutoexecJobPhaseStatusApi.java
+++ src/main/java/neatlogic/module/autoexec/api/job/exec/UpdateAutoexecJobPhaseStatusApi.java
@@ -16,6 +16,7 @@ limitations under the License.
 
 package neatlogic.module.autoexec.api.job.exec;
 
+import com.alibaba.fastjson.JSONObject;
 import neatlogic.framework.asynchronization.thread.NeatLogicThread;
 import neatlogic.framework.asynchronization.threadlocal.UserContext;
 import neatlogic.framework.asynchronization.threadpool.TransactionSynchronizationPool;
@@ -24,11 +25,13 @@ import neatlogic.framework.autoexec.auth.AUTOEXEC_BASE;
 import neatlogic.framework.autoexec.constvalue.JobNodeStatus;
 import neatlogic.framework.autoexec.constvalue.JobPhaseStatus;
 import neatlogic.framework.autoexec.dao.mapper.AutoexecJobMapper;
-import neatlogic.framework.autoexec.dto.AutoexecJobSourceVo;
 import neatlogic.framework.autoexec.dto.job.AutoexecJobPhaseRunnerVo;
 import neatlogic.framework.autoexec.dto.job.AutoexecJobPhaseVo;
 import neatlogic.framework.autoexec.dto.job.AutoexecJobVo;
-import neatlogic.framework.autoexec.exception.*;
+import neatlogic.framework.autoexec.exception.AutoexecJobNotFoundException;
+import neatlogic.framework.autoexec.exception.AutoexecJobPhaseNotFoundException;
+import neatlogic.framework.autoexec.exception.AutoexecJobRunnerNotFoundException;
+import neatlogic.framework.autoexec.exception.AutoexecJobSourceInvalidException;
 import neatlogic.framework.autoexec.job.source.type.AutoexecJobSourceTypeHandlerFactory;
 import neatlogic.framework.autoexec.job.source.type.IAutoexecJobSourceTypeHandler;
 import neatlogic.framework.autoexec.source.AutoexecJobSourceFactory;
@@ -43,7 +46,6 @@ import neatlogic.framework.restful.annotation.*;
 import neatlogic.framework.restful.constvalue.OperationTypeEnum;
 import neatlogic.framework.restful.core.privateapi.PrivateApiComponentBase;
 import neatlogic.module.autoexec.service.AutoexecJobService;
-import com.alibaba.fastjson.JSONObject;
 import org.apache.commons.collections4.MapUtils;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -207,7 +209,7 @@ public class UpdateAutoexecJobPhaseStatusApi extends PrivateApiComponentBase {
         }
 
         //如果状态一致或者状态已经是失败，则无需更新状态，防止多次触发callback；
-        if (Objects.equals(jobVo.getStatus(), finalJobPhaseStatus) || JobPhaseStatus.FAILED.getValue().equals(jobVo.getStatus())) {
+        if (Objects.equals(jobVo.getStatus(), finalJobPhaseStatus) || (Objects.equals(jobVo.getStatus(), finalJobPhaseStatus) && JobPhaseStatus.FAILED.getValue().equals(jobVo.getStatus()))) {
             return;
         }
         //autoexec是不会回调failed的作业状态，故如果存在失败的phase 则更新作业状态为failed
