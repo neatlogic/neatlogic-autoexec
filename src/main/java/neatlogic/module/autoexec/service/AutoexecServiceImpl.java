de057a833808b17ce0e5de52d9f3507d5c66faf1
1437892690@qq.com <1437892690@qq.com>
2024-04-26 18:32:32 +0800

    [修复] 组合工具-组合工具有作业参数，且参数必填，保存组合工具异常
    关联 #[1143530179756032]组合工具-组合工具有作业参数，且参数必填，保存组合工具异常 http://192.168.0.96:8090/demo/rdm.html#/bug-detail/939050947543040/939050947543057/1143530179756032

--- src/main/java/neatlogic/module/autoexec/service/AutoexecServiceImpl.java
+++ src/main/java/neatlogic/module/autoexec/service/AutoexecServiceImpl.java
@@ -640,7 +640,6 @@ public class AutoexecServiceImpl implements AutoexecService, IAutoexecServiceCro
             return true;
         }
         for (int i = 0; i < validateList.size(); i++) {
-//            JSONObject validateObj = validateList.getJSONObject(i);
             Object validate = validateList.get(i);
             if (validate == null) {
                 continue;
