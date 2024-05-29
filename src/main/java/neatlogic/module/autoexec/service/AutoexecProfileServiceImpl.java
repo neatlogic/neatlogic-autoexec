4e79384e414641722c697f2796e57e1d86c1e853
lvzk <897706680@qq.com>
2024-05-29 14:49:54 +0800

    [修复]自动化被引用的全局参数被删除，导致null异常

--- src/main/java/neatlogic/module/autoexec/service/AutoexecProfileServiceImpl.java
+++ src/main/java/neatlogic/module/autoexec/service/AutoexecProfileServiceImpl.java
@@ -185,11 +185,13 @@ public class AutoexecProfileServiceImpl implements AutoexecProfileService, IAuto
             if (StringUtils.equals(paramVo.getMappingMode(), AutoexecProfileParamInvokeType.GLOBAL_PARAM.getValue())) {
                 //获取引用的全局参数值
                 AutoexecGlobalParamVo globalParamVo = autoexecGlobalParamMapper.getGlobalParamByKey(paramVo.getDefaultValueStr());
-                if (StringUtils.equals(AutoexecGlobalParamType.PASSWORD.getValue(), globalParamVo.getType()) && globalParamVo.getDefaultValue() != null) {
-                    String pwd = RC4Util.encrypt(globalParamVo.getDefaultValueStr());
-                    paramVo.setDefaultValue(pwd);
-                } else {
-                    paramVo.setDefaultValue(globalParamVo.getDefaultValue());
+                if(globalParamVo != null) {
+                    if (StringUtils.equals(AutoexecGlobalParamType.PASSWORD.getValue(), globalParamVo.getType()) && globalParamVo.getDefaultValue() != null) {
+                        String pwd = RC4Util.encrypt(globalParamVo.getDefaultValueStr());
+                        paramVo.setDefaultValue(pwd);
+                    } else {
+                        paramVo.setDefaultValue(globalParamVo.getDefaultValue());
+                    }
                 }
             }
             returnMap.put(paramVo.getKey(), paramVo.getDefaultValue());
