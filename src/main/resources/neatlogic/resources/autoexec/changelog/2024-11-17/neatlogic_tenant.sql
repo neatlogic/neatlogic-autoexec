44ba19f057bb6f4372e0e6aee116bff4915bd578
lvzk <897706680@qq.com>
2024-11-21 18:29:17 +0800

    [功能]自动化作业批量插入节点性能优化 #[1293323044618240]自动化作业批量插入节点性能优化 http://192.168.0.96:8090/demo/rdm.html#/story-detail/939050947543040/939050947543042/1293323044618240

--- src/main/resources/neatlogic/resources/autoexec/changelog/2024-11-17/neatlogic_tenant.sql
+++ src/main/resources/neatlogic/resources/autoexec/changelog/2024-11-17/neatlogic_tenant.sql
@@ -10,14 +10,14 @@ ALTER TABLE `autoexec_job_phase_node` DROP INDEX `idx_lcd`;
 
 ALTER TABLE `autoexec_job_phase_node` DROP INDEX `idx_resource_id_job_phase_id`;
 
-ALTER TABLE `autoexec_job_phase_node` ADD INDEX `idx_phaseid_updatetag`(`job_phase_id`, `update_tag`) USING BTREE;
-
 ALTER TABLE `autoexec_job_phase_node` ADD COLUMN `runner_map_id` bigint NULL COMMENT '执行器id' AFTER `is_executed`;
 
 ALTER TABLE `autoexec_job_phase_node` ADD COLUMN `update_tag` bigint NULL COMMENT '最近一次更新标记' AFTER `runner_map_id`;
 
-ALTER TABLE `autoexec_job_phase_node` ADD UNIQUE INDEX `idx_phaseid_resourceid`(`job_phase_id`, `resource_id`) USING BTREE;
-
 ALTER TABLE `autoexec_job_phase_node` ADD COLUMN `error_type` int NULL COMMENT '异常信息' AFTER `update_tag`;
 
 ALTER TABLE `autoexec_job_phase_node` MODIFY COLUMN `status` enum('succeed','pending','failed','ignored','running','aborted','aborting','waitInput','pausing','paused','invalid') CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '状态' AFTER `end_time`;
+
+ALTER TABLE `autoexec_job_phase_node` ADD INDEX `idx_phaseid_updatetag`(`job_phase_id`, `update_tag`) USING BTREE;
+
+ALTER TABLE `autoexec_job_phase_node` ADD UNIQUE INDEX `idx_phaseid_resourceid`(`job_phase_id`, `resource_id`) USING BTREE;
