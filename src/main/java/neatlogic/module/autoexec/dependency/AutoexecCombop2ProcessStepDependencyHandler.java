cce94fbdfc824fbddc3136cf3308753e7620ea88
1437892690@qq.com <1437892690@qq.com>
2024-12-26 11:37:06 +0800

    [功能] 组合工具添加引用列表
    关联 #[1318905102958592]组合工具添加引用列表 http://192.168.0.96:8090/demo/rdm.html#/task-detail/939050947543040/939050947543050/1318905102958592

--- src/main/java/neatlogic/module/autoexec/dependency/AutoexecCombop2ProcessStepDependencyHandler.java
+++ src/main/java/neatlogic/module/autoexec/dependency/AutoexecCombop2ProcessStepDependencyHandler.java
@@ -15,95 +15,80 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.*/
 
 package neatlogic.module.autoexec.dependency;
 
-import com.alibaba.fastjson.JSONArray;
-import com.alibaba.fastjson.JSONObject;
-import neatlogic.framework.asynchronization.threadlocal.TenantContext;
 import neatlogic.framework.autoexec.constvalue.AutoexecFromType;
-import neatlogic.framework.crossover.CrossoverServiceFactory;
 import neatlogic.framework.dependency.core.DefaultDependencyHandlerBase;
 import neatlogic.framework.dependency.core.IFromType;
 import neatlogic.framework.dependency.dto.DependencyInfoVo;
 import neatlogic.framework.dependency.dto.DependencyVo;
-import neatlogic.framework.process.crossover.IProcessCrossoverMapper;
-import neatlogic.framework.process.dto.ProcessVo;
-import neatlogic.module.autoexec.process.dto.CreateJobConfigConfigVo;
-import neatlogic.module.autoexec.process.dto.CreateJobConfigVo;
-import org.apache.commons.collections4.CollectionUtils;
-import org.apache.commons.collections4.MapUtils;
-import org.springframework.stereotype.Component;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
-
-@Component
+//@Component
 public class AutoexecCombop2ProcessStepDependencyHandler extends DefaultDependencyHandlerBase {
 
     @Override
     protected DependencyInfoVo parse(DependencyVo dependencyVo) {
-        JSONObject config = dependencyVo.getConfig();
-        if (MapUtils.isEmpty(config)) {
-            return null;
-        }
-        String stepUuid = dependencyVo.getTo();
-        String processUuid = config.getString("processUuid");
-        IProcessCrossoverMapper processCrossoverMapper = CrossoverServiceFactory.getApi(IProcessCrossoverMapper.class);
-        ProcessVo processVo = processCrossoverMapper.getProcessBaseInfoByUuid(processUuid);
-        if (processVo == null) {
-            return null;
-        }
-        JSONObject processConfig = processVo.getConfig();
-        if (MapUtils.isEmpty(processConfig)) {
-            return null;
-        }
-        JSONObject processObj = processConfig.getJSONObject("process");
-        if (MapUtils.isEmpty(processObj)) {
-            return null;
-        }
-        JSONArray stepList = processObj.getJSONArray("stepList");
-        if (CollectionUtils.isEmpty(stepList)) {
-            return null;
-        }
-        for (int i = 0; i < stepList.size(); i++) {
-            JSONObject stepObj = stepList.getJSONObject(i);
-            if (MapUtils.isEmpty(stepObj)) {
-                continue;
-            }
-            String uuid = stepObj.getString("uuid");
-            if (!Objects.equals(uuid, stepUuid)) {
-                continue;
-            }
-            JSONObject stepConfig = stepObj.getJSONObject("stepConfig");
-            if (MapUtils.isEmpty(stepConfig)) {
-                return null;
-            }
-            JSONObject createJobConfig = stepConfig.getJSONObject("createJobConfig");
-            if (MapUtils.isEmpty(createJobConfig)) {
-                return null;
-            }
-            boolean flag = false;
-            CreateJobConfigVo createJobConfigVo = createJobConfig.toJavaObject(CreateJobConfigVo.class);
-            List<CreateJobConfigConfigVo> configList = createJobConfigVo.getConfigList();
-            if (CollectionUtils.isNotEmpty(configList)) {
-                for (CreateJobConfigConfigVo createJobConfigConfigVo : configList) {
-                    if (createJobConfigConfigVo.getCombopId() != null && Objects.equals(createJobConfigConfigVo.getCombopId().toString(), dependencyVo.getFrom())) {
-                        flag = true;
-                        break;
-                    }
-                }
-            }
-            if (!flag) {
-                return null;
-            }
-            JSONObject dependencyInfoConfig = new JSONObject();
-            dependencyInfoConfig.put("processUuid", processUuid);
-            dependencyInfoConfig.put("stepUuid", stepUuid);
-            List<String> pathList = new ArrayList<>();
-            pathList.add("流程管理");
-            pathList.add(processVo.getName());
-            String urlFormat = "/" + TenantContext.get().getTenantUuid() + "/process.html#/flow-edit?uuid=${DATA.processUuid}&stepUuid=${DATA.stepUuid}";
-            return new DependencyInfoVo(stepUuid, dependencyInfoConfig, stepObj.getString("name"), pathList, urlFormat, this.getGroupName());
-        }
+//        JSONObject config = dependencyVo.getConfig();
+//        if (MapUtils.isEmpty(config)) {
+//            return null;
+//        }
+//        String stepUuid = dependencyVo.getTo();
+//        String processUuid = config.getString("processUuid");
+//        IProcessCrossoverMapper processCrossoverMapper = CrossoverServiceFactory.getApi(IProcessCrossoverMapper.class);
+//        ProcessVo processVo = processCrossoverMapper.getProcessBaseInfoByUuid(processUuid);
+//        if (processVo == null) {
+//            return null;
+//        }
+//        JSONObject processConfig = processVo.getConfig();
+//        if (MapUtils.isEmpty(processConfig)) {
+//            return null;
+//        }
+//        JSONObject processObj = processConfig.getJSONObject("process");
+//        if (MapUtils.isEmpty(processObj)) {
+//            return null;
+//        }
+//        JSONArray stepList = processObj.getJSONArray("stepList");
+//        if (CollectionUtils.isEmpty(stepList)) {
+//            return null;
+//        }
+//        for (int i = 0; i < stepList.size(); i++) {
+//            JSONObject stepObj = stepList.getJSONObject(i);
+//            if (MapUtils.isEmpty(stepObj)) {
+//                continue;
+//            }
+//            String uuid = stepObj.getString("uuid");
+//            if (!Objects.equals(uuid, stepUuid)) {
+//                continue;
+//            }
+//            JSONObject stepConfig = stepObj.getJSONObject("stepConfig");
+//            if (MapUtils.isEmpty(stepConfig)) {
+//                return null;
+//            }
+//            JSONObject createJobConfig = stepConfig.getJSONObject("createJobConfig");
+//            if (MapUtils.isEmpty(createJobConfig)) {
+//                return null;
+//            }
+//            boolean flag = false;
+//            CreateJobConfigVo createJobConfigVo = createJobConfig.toJavaObject(CreateJobConfigVo.class);
+//            List<CreateJobConfigConfigVo> configList = createJobConfigVo.getConfigList();
+//            if (CollectionUtils.isNotEmpty(configList)) {
+//                for (CreateJobConfigConfigVo createJobConfigConfigVo : configList) {
+//                    if (createJobConfigConfigVo.getCombopId() != null && Objects.equals(createJobConfigConfigVo.getCombopId().toString(), dependencyVo.getFrom())) {
+//                        flag = true;
+//                        break;
+//                    }
+//                }
+//            }
+//            if (!flag) {
+//                return null;
+//            }
+//            JSONObject dependencyInfoConfig = new JSONObject();
+//            dependencyInfoConfig.put("processUuid", processUuid);
+//            dependencyInfoConfig.put("stepUuid", stepUuid);
+//            List<String> pathList = new ArrayList<>();
+//            pathList.add("流程管理");
+//            pathList.add(processVo.getName());
+//            String urlFormat = "/" + TenantContext.get().getTenantUuid() + "/process.html#/flow-edit?uuid=${DATA.processUuid}&stepUuid=${DATA.stepUuid}";
+//            return new DependencyInfoVo(stepUuid, dependencyInfoConfig, stepObj.getString("name"), pathList, urlFormat, this.getGroupName());
+//        }
         return null;
     }
 
