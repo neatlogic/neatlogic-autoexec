4f99fe5b4801cd4064a35eb73165e15d0df3b6bb
lvzk <897706680@qq.com>
2025-04-16 15:39:02 +0800

    [修复]不同发布作业执行sql异常

--- src/main/java/neatlogic/module/autoexec/api/job/action/node/UpdateAutoexecJobSqlApi.java
+++ src/main/java/neatlogic/module/autoexec/api/job/action/node/UpdateAutoexecJobSqlApi.java
@@ -1,5 +1,6 @@
 package neatlogic.module.autoexec.api.job.action.node;
 
+import com.alibaba.fastjson.JSONObject;
 import neatlogic.framework.auth.core.AuthAction;
 import neatlogic.framework.autoexec.auth.AUTOEXEC_MODIFY;
 import neatlogic.framework.autoexec.dao.mapper.AutoexecJobMapper;
@@ -7,12 +8,9 @@ import neatlogic.framework.autoexec.exception.AutoexecJobNotFoundException;
 import neatlogic.framework.autoexec.job.source.type.AutoexecJobSourceTypeHandlerFactory;
 import neatlogic.framework.autoexec.job.source.type.IAutoexecJobSourceTypeHandler;
 import neatlogic.framework.common.constvalue.ApiParamType;
-import neatlogic.framework.deploy.constvalue.JobSourceType;
 import neatlogic.framework.restful.annotation.*;
 import neatlogic.framework.restful.constvalue.OperationTypeEnum;
 import neatlogic.framework.restful.core.privateapi.PrivateApiComponentBase;
-import com.alibaba.fastjson.JSONObject;
-import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -60,14 +58,8 @@ public class UpdateAutoexecJobSqlApi extends PrivateApiComponentBase {
         if (autoexecJobMapper.getJobLockByJobId(paramObj.getLong("jobId")) == null) {
             throw new AutoexecJobNotFoundException(paramObj.getLong("jobId"));
         }
-        IAutoexecJobSourceTypeHandler handler;
-        if (StringUtils.equals(paramObj.getString("operType"), neatlogic.framework.autoexec.constvalue.JobSourceType.AUTOEXEC.getValue())) {
-            handler = AutoexecJobSourceTypeHandlerFactory.getAction(neatlogic.framework.autoexec.constvalue.JobSourceType.AUTOEXEC.getValue());
-            handler.updateSqlStatus(paramObj);
-        } else if (StringUtils.equals(paramObj.getString("operType"), JobSourceType.DEPLOY.getValue())) {
-            handler = AutoexecJobSourceTypeHandlerFactory.getAction(JobSourceType.DEPLOY.getValue());
-            handler.updateSqlStatus(paramObj);
-        }
+        IAutoexecJobSourceTypeHandler handler = AutoexecJobSourceTypeHandlerFactory.getAction(paramObj.getString("operType"));
+        handler.updateSqlStatus(paramObj);
         return null;
     }
 }
