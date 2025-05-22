90b4146a61a6cb8b4f75500c93732670200a3768
1437892690@qq.com <1437892690@qq.com>
2025-05-22 10:20:34 +0800

    [修复] error日志报错8
    关联 #[1426664322465792]error日志报错8 http://192.168.0.96:8090/demo/rdm.html#/bug-detail/939050947543040/939050947543057/1426664322465792

--- src/main/java/neatlogic/module/autoexec/service/AutoexecJobActionServiceImpl.java
+++ src/main/java/neatlogic/module/autoexec/service/AutoexecJobActionServiceImpl.java
@@ -64,7 +64,6 @@ import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.Resource;
@@ -454,7 +453,7 @@ public class AutoexecJobActionServiceImpl implements AutoexecJobActionService, I
     }
 
     @Override
-    @Transactional(propagation = Propagation.REQUIRES_NEW)
+    @Transactional
     public void validateCreateJob(AutoexecJobVo jobParam) throws Exception {
         validateAndCreateJobFromCombop(jobParam);
         jobParam.setAction(JobAction.FIRE.getValue());
